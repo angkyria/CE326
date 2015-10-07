0a0e2522df27ca19db9b109905b4f84eb333eb9c
Angelos Kyriacou <angkyria@podilates.com>
2015-10-07 20:25:39 +0300

    pipe with thread for string

--- homework_1/1_1/thread_pipe/test.c
+++ homework_1/1_1/thread_pipe/test.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-
+#define MAX_BUF 30
 int fd[2];
 int anamoni=0;
 
 void *pipe_read(){
-	char ch;
+	char ch[MAX_BUF];
 	int nread;
         while(1){
+		memset(ch,0,sizeof(ch));
 		while(anamoni==0){};
-		nread = read(fd[0],&ch,1);
+		nread = read(fd[0],ch,sizeof(ch));
 		if(nread == -1){
 		        perror("fail read");
 		        exit(3);
 	        }
-		printf("Read pipeRead char %c\n", ch);
+		printf("Read pipeRead char %s\n", ch);
 		sleep(3);
+		anamoni=0;
 	}
 }
 
 void *pipe_write(){
-	int nwrite;
-	char ch='A';
-
+	int nwrite, i;
+	char ch[MAX_BUF];
+	
 	while(1){
-		nwrite=write(fd[1],&ch,1);
-		if(nwrite!=1){
+
+ 		memset(ch, 0, sizeof(ch));
+		scanf("%s", ch); 
+ 		nwrite=write(fd[1],ch,strlen(ch));
+		if(nwrite==-1){
 			perror("fail write");
 			exit(2);
 		}
 
-		printf("pipe write %c\n", ch);
-		if(ch=='Z'){
-			ch = 'A'-1;
-		}
-		ch++;
+		printf("pipe write %s\n", ch);
 		anamoni=1;
 		sleep(3);
 	}
