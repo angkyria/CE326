2ccd1ded0cf42b5a2eccf42a835e471e45a299d4
Panayiotis <panagiotispetrou91@gmail.com>
2015-10-07 16:13:56 +0300

    fgdsf

---
+++ 1_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+
+void pipe_init(int size);
+void pipe_write(char c);
+void pipr_close();
+int pipe_read(char *c);
+ 
+  //int in;
+ // int out;
+  
+int main(int argc,char* argv[]){
+  char str[SIZE];
+ 
+  int array_size;
+  int i;
+  char fifo[SIZE];
+  
+  printf("enter size of array: \n");
+  scanf ("%d",&array_size );
+  
+  pipe_init(array_size);
+  
+  printf("enter data: \n");
+  scanf ("\t%s",str );
+  
+  for(i=0;str[i]!='\0';i++){
+    pipe_write(str[i]);
+  }
+  
+  return 0;
+}
+pipe_init(int size){
+  int i;
+  for(i=0;i<size;i++){
+    fifo[i]=0;
+    
+  }
+}
+void pipe_write(char c){
+  
+}
