645bc6679f844ffb7ddd0eaba6b17576056f6aff
Angelos Kyriacou <angkyria@podilates.com>
2015-10-21 17:21:28 +0300

    mandel thread

---
+++ homework_1/1_2/mandelCore.h
@@ -0,0 +1,16 @@
+typedef struct {
+  long double reBeg,reInc;  /* beginning and increment for real values */
+  long double imBeg,imInc;  /* beginning and increment for imaginary values */
+  int reSteps,imSteps;           /* number of steps/points per range */
+} mandel_Pars;
+
+extern void mandel_Slices(mandel_Pars *pars, int n, mandel_Pars slices[]);
+/* len(slices) == n */
+
+extern void mandel_Calc(mandel_Pars *pars, int maxIterations, int res[]);
+/* len(res) = pars->reSteps*pars->imSteps */
+
+typede struct{
+   mandel_Pars       ;
+   int maxIterations;
+}
