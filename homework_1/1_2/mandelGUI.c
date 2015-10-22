4ed7f8e39f810ce8bedf37972362d54b066fe287
Angelos Kyriacou <angkyria@podilates.com>
2015-10-22 22:57:45 +0300

    slice draw point with threads

--- homework_1/1_2/mandelGUI.c
+++ homework_1/1_2/mandelGUI.c
@@ -213,7 +213,7 @@ int main(int argc, char *argv[]) {
         for(k=0;work_status[k]!=1;k++)if((k+1)==nofslices)j=-1;
 
 
-        for(work_status[k]=2, k=0;x<slices[k].imSteps;k++,y++,i++){
+        for(work_status[k]=2, j=0;x<slices[k].imSteps;j++,y++,i++){
             for(x=0;x<slices[k].reSteps;x++){
                 setColor(pickColor(res[x*slices[k].reSteps+x],maxIterations));
                 drawPoint(x, y);
