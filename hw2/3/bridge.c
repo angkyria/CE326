f99fddaa82c5f160df1d658a5f907a9cc2fb55f9
Angelos Kyriacou <angkyria@podilates.com>
2015-11-09 19:09:45 +0200

    add some checking

--- hw2/3/bridge.c
+++ hw2/3/bridge.c
@@ -37,6 +37,11 @@ int main(int argc, char *argv[]){
         if( ( c == 'l') || ( c == 'L') ){
 
             car_tail = (char *) realloc( car_tail, (i+1) * sizeof(char));
+	    if(car_tail == NULL){
+		    free(car_tail);
+		    perror("Error reallocating memory");
+		    exit(3);
+	    }
             car_tail[i] = 'l';
             size_of_left_tail++;
             i++;
@@ -100,6 +105,17 @@ int main(int argc, char *argv[]){
     }
 
     car = (pthread_t *) malloc(size_of_car_tail*sizeof(pthread_t));//allocate number of car threads
+    if (car == NULL){  
+	pthread_mutex_destroy(&mtxend);
+        pthread_mutex_destroy(&mtx_dir_pri);
+        pthread_mutex_destroy(&mtx_l_dir);
+        pthread_mutex_destroy(&mtx_r_dir);
+	pthread_mutex_destroy(&mtxleave);
+        free(car_tail);
+        perror("Fail allocate car treads\n");
+        exit(2);                             
+    }	
+	    
     for(i=0;i<size_of_car_tail;i++){
         thread_status=pthread_create(&car[i],NULL, car_arrive, (void*)(intptr_t)i);//start n car thread
         if(thread_status){
